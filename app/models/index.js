f4863d11f22d92c2f583733f393a54118e12ec0f
XiaoMing <56507353+15960049901@users.noreply.github.com>
2021-05-05 22:39:36 +0800

    初始化项目

---
+++ app/models/index.js
@@ -0,0 +1,28 @@
+// 导入数据库配置文件
+const dbConfig = require("../config/db.config.js");
+
+// 引入Sequelize框架
+const Sequelize = require("sequelize");
+// 创建数据库连接
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
